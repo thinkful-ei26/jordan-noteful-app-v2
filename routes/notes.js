14012654c882d66163369782593a55073e6a75b9
Jordan <jordanhaddadi@gmail.com>
2018-11-26 12:10:24 -0500

    setup heroku

--- routes/notes.js
+++ routes/notes.js
@@ -14,21 +14,6 @@ router.get('/', (req, res, next) => {
   const searchTerm = req.query.searchTerm;
   const folderId = req.query.id;
 
-//   knex.select('id', 'title', 'content')
-//     .from('notes')
-//     .modify(function (queryBuilder) {
-//       if (searchTerm) {
-//         queryBuilder.where('title', 'like', `%${searchTerm}%`);
-//       }
-//     })
-//     .orderBy('notes.id')
-//     .then(results => {
-//       res.json(results);
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// });
 
 knex.select('notes.id', 'title', 'content', 'folders.id as folderId', 'folders.name as folderName')
   .from('notes')
@@ -50,7 +35,6 @@ knex.select('notes.id', 'title', 'content', 'folders.id as folderId', 'folders.n
   .catch(err => next(err));
 });
 
-
 // Get a single item by id
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
@@ -59,8 +43,8 @@ router.get('/:id', (req, res, next) => {
     .select('notes.id', 'title', 'content', 'folders.id as folderId', 'folders.name as folderName')
     .from('notes')
     .leftJoin('folders', 'notes.folder_id', 'folders.id')
-    .where('id', `${id}`)
-    .returning('id')
+    .where('notes.id', `${id}`)
+    .returning('notes.id')
     .then(results => {
       res.json(results[0]);
     })
@@ -69,17 +53,58 @@ router.get('/:id', (req, res, next) => {
     }); 
 });
 
+// Creat a new item
+router.post('/', (req, res, next) => {
+  const { title, content, folderId } = req.body; // Add `folderId` to object destructure
+  
+  const newItem = {
+    title: title,
+    content: content,
+    folder_id: folderId  // Add `folderId`
+  };
+
+  let noteId;
+
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  // Insert new note, instead of returning all the fields, just return the new `id`
+  knex('notes')
+    .returning('id')
+    .insert(newItem)
+    .then(([id]) => {
+      noteId = id;
+      // Using the new id, select the new note and the folder
+      return knex.select('notes.id', 'title', 'content', 'folder_id as folderId', 'folders.name as folderName')
+        .from('notes')
+        .leftJoin('folders', 'notes.folder_id', 'folders.id')
+        .where('notes.id', noteId);
+    })
+    .then(([result]) => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => next(err));
+});
+
 // Put update an item
 router.put('/:id', (req, res, next) => {
   const id = req.params.id;
   /***** Never trust users - validate input *****/
   const updateObj = {};
-  const updateableFields = ['title', 'content'];
+  const updateableFields = ['title', 'content', 'folderId'];
 
   updateableFields.forEach(field => 
-   { if (field in req.body) 
+   { if (field in req.body){
+      if(field === 'folderId'){
+        updateObj['folder_id'] = req.body[field];
+      } else
       { updateObj[field] = req.body[field]; } 
-  })
+  }
+}); 
 
    /***** Never trust users - validate input *****/
    if (!updateObj.title) {
@@ -88,10 +113,11 @@ router.put('/:id', (req, res, next) => {
     return next(err);
   }
 
-  knex
-    .select('notes.id', 'title', 'content')
-    .from('notes')
+  knex('notes')
+    .select('notes.id', 'title', 'content', 'folders.id as folderId', 'folders.name as folderName')
     .where('id', `${id}`)
+    .update(updateObj)
+    .leftJoin('folders', 'notes.folder_id', 'folders.id')
     .returning(['notes.id', 'title', 'content'])
     .update(updateObj)
       .then(results => {
@@ -101,29 +127,6 @@ router.put('/:id', (req, res, next) => {
         next(err);
       }); 
   });
-
-// Post (insert) an item
-router.post('/', (req, res, next) => {
-  const { title, content } = req.body;
-
-  const newItem = { title, content };
-  /***** Never trust users - validate input *****/
-  if (!newItem.title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err);
-  }
-
-  knex('notes')
-    .returning(['notes.id', 'title', 'content'])
-    .insert(`${newItem}`)
-    .then(results => {
-      res.json(results);
-    })
-    .catch(err => {
-      next(err);
-      });
-});
 
 // Delete an item
 router.delete('/:id', (req, res, next) => {
