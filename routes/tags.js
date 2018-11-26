3d15ffbdc083fc369f3544349a93d1fa7faec18d
Jordan <jordanhaddadi@gmail.com>
2018-11-26 12:53:47 -0500

    tags router

--- routes/tags.js
+++ routes/tags.js
@@ -7,8 +7,37 @@ const router = express.Router();
 
 const knex = require('../knex');
 
-/* ========== POST/CREATE ITEM ========== */
-router.post('/tags', (req, res, next) => {
+/* ========== GET ALL TAGS =========== */
+router.get('/', (req, res, next) => {
+  
+  knex.select('id', 'name')
+    .from('tags')
+    .orderBy('tags.id')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+  });
+  
+/* ========== GET TAG BY ID =========== */
+router.get('/:id', (req, res, next) => {
+    const id = req.params.id;
+  
+    knex  
+      .select()
+      .from('tags')
+      .where('id', `${id}`)
+      .returning('id')
+      .then(results => {
+        res.json(results[0]);
+      })
+      .catch(err => {
+        next(err);
+      }); 
+  }); 
+
+/* ========== POST/CREATE TAG ========== */
+router.post('/', (req, res, next) => {
     const { name } = req.body;
   
     /***** Never trust users. Validate input *****/
@@ -31,8 +60,54 @@ router.post('/tags', (req, res, next) => {
       .catch(err => next(err));
   });
 
+/* ========== UPDATE TAG ========== */
+router.put('/:id', (req, res, next) => {
 
+    const id = req.params.id;
+/***** Never trust users - validate input *****/
+    const updateObj = {};
+    const updateableFields = ['id', 'name'];
 
+updateableFields.forEach(field => 
+    { if (field in req.body) 
+    { updateObj[field] = req.body[field]; } 
+    })
 
+    /***** Never trust users - validate input *****/
+    if (!updateObj.name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+}
+
+knex
+    .select('tags.id', 'id', 'name')
+    .from('tags')
+    .where('id', `${id}`)
+    .returning(['tags.id', 'name'])
+    .update(updateObj)
+    .then(results => {
+        res.json(results[0]);
+    })
+    .catch(err => {
+        next(err);
+    }); 
+});
+
+/* ========== DELETE TAG ========== */
+
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    
+    knex('tags')
+        .where({'id': `${id}`})
+        .del()
+        .then(results => {
+        res.json(results);
+        })
+        .catch(err => {
+        next(err);
+        });
+    });
 
 module.exports = router;
